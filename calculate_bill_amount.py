babf9bb2e2861be3a648038e953230e34288d4f7
Md Mazharul Haque <cpon143@gmail.com>
2022-08-17 22:54:46 +0530

    new change

--- calculate_bill_amount.py
+++ calculate_bill_amount.py
@@ -5,16 +5,16 @@ value = float(input("Enter value: "))
 discount = float(input("discount: "))
 tax = float(input("Enter tax amount: "))
 
+
 total_cost=quantity_sold*value
 print('total_cost',total_cost)
+
 dis_amt=((total_cost*discount)/100)
 print('dis_amt',dis_amt)
-dis_cost=quantity_sold-dis_amt
+
+dis_cost=total_cost-dis_amt
 print('dis_cost',dis_cost)
+
 tax_amt=((tax*dis_cost)/100)
 print('tax_amt',tax_amt)
-
-
-
-
-5
+print('Bill-Amount',(dis_cost+tax_amt))
