278d84cf752db12c822e23a4954599e11c8bf9f5
Md Mazharul Haque <cpon143@gmail.com>
2022-08-24 13:54:57 +0530

    new changes

---
+++ for_loop.py
@@ -0,0 +1,30 @@
+# counter controled loop
+# for i in range (1, 11) or i<11
+
+
+#  sum of 1st 10 natural number 
+
+sum=0
+for i in range(1, 11):
+    sum=sum+i 
+    print(sum)
+    
+print('*******************')
+    
+# range (s  ,e  ,d )
+    # (start, end, step)
+    
+for i in range(1,25,2):
+    print(i)
+ 
+ 
+print('*******************')
+   
+
+# negative 
+
+for i in range(10,0,-1):
+    print(i)
+    
+    
+print('*******************')
