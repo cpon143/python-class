a41d4d5af1cdca2f4fb41ead6922a44625fb73e0
Md Mazharul Haque <cpon143@gmail.com>
2022-08-29 21:53:31 +0530

    new changes

---
+++ cool-shapes.py
@@ -0,0 +1,15 @@
+from turtle import *
+
+bgcolor('Black')
+speed(0)
+hideturtle()
+for i in range(1200):
+    color('green')
+    circle(i)
+    color('blue')
+    circle(5*5)
+    right(30)
+    forward(3)
+done()
+
+    
