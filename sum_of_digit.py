404260f19fd72f8f3be66ba66f8cdb801947d98d
Md Mazharul Haque <cpon143@gmail.com>
2022-09-01 16:47:54 +0530

    new changes

--- sum_of_digit.py
+++ sum_of_digit.py
@@ -6,3 +6,15 @@ while(n!=0):
     sum=sum+t
     n=n//10
 print(sum)
+
+
+def add_digit(a:int):
+    sum=0
+    while(a!=0):
+        t=a%10
+        sum=sum+t
+        a=a//10
+    print(sum)
+a=int(input("which digit do you want to sum"))
+add_digit(a)
+
