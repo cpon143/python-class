d89c7bc1a3ddd1abe854f07c89c4e9389a9edbc6
Md Mazharul Haque <cpon143@gmail.com>
2022-09-01 15:29:39 +0530

    new changes

--- function_simple.py
+++ function_simple.py
@@ -1,4 +1,12 @@
-def simple():
-    print("Hello in_side function")
-print("Hello out_side function")
-simple()
+# def simple():
+#     print("Hello in_side function")
+# print("Hello out_side function")
+# simple()
+
+def add():
+    a=5
+    b=10
+    sum=a+b
+    print(sum)
+print("first before function ")
+add()
