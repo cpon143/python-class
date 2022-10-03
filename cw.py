696e22104bbc03573bbe12c07c92c02bd7d5d8d1
Md Mazharul Haque <cpon143@gmail.com>
2022-10-03 22:39:21 +0530

    new change

---
+++ cw.py
@@ -0,0 +1,9 @@
+#wap to create a class animal with some common attributes with defined function, further create attribute
+# atleast two classes herbivores & carnivorus show casing function overloading from base class animal describing about
+# the full thing eat
+x = int(input(""))
+print( "Please enter a number")
+if (x > 9 and x < 100) or (x < -9 and x > -100):
+    print ("Double")
+else:
+    print ("Single")
